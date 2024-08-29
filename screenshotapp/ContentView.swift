daaebecaaa68c50fda602d2175d01ad538380114
jocelinqueau <queau.j@gmail.com>
2024-08-29 16:27:57 +0200

    Initial Commit

---
+++ screenshotapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  screenshotapp
+//
+//  Created by Jocelin Quéau on 29/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ screenshotapp/screenshotappApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenshotappApp.swift
+//  screenshotapp
+//
+//  Created by Jocelin Quéau on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct screenshotappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
