b6daa8ddbfe099b00717a1967bcc64cfd252eff9
jocelinqueau <queau.j@gmail.com>
2024-08-29 16:58:24 +0200

    refacto and handle multiple images

--- screenshotapp/ContentView.swift
+++ screenshotapp/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var vm = ScreencaptureViewModel()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            ForEach(vm.images, id:\.self){ image in
+                Image(nsImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            Button("Make a screenshot"){
+                vm.takeScreenshot()
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
--- screenshotapp/ScreencaptureViewModel.swift
+++ screenshotapp/ScreencaptureViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by Jocelin Quéau on 29/08/2024.
 //
 
-import Foundation
+import SwiftUI
+
+class ScreencaptureViewModel:ObservableObject{
+    
+    @Published var images = [NSImage]()
+    
+    func takeScreenshot(){
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: "/usr/sbin/screencapture")
+        task.arguments = ["-cw"]
+        
+        do {
+            try task.run()
+            task.waitUntilExit()
+            getImageFromPasteboard()
+            
+        }catch{
+            print("could not take screenshot: \(error)")
+        }
+    }
+    
+    private func getImageFromPasteboard(){
+        guard NSPasteboard.general.canReadItem(withDataConformingToTypes: NSImage.imageTypes) else {
+            return
+        }
+        
+        guard let image = NSImage(pasteboard: NSPasteboard.general) else {
+            return
+        }
+        
+        self.images.append(image)
+    }
+}
